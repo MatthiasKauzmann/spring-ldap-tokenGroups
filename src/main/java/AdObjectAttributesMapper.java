4355fcd5bbed14953b52779554baefb8fc34c65a
Matthias Kauzmann <37530569+MatthiasKauzmann@users.noreply.github.com>
2020-03-29 12:26:13 +0200

    Update AdObjectAttributesMapper.java

--- src/main/java/AdObjectAttributesMapper.java
+++ src/main/java/AdObjectAttributesMapper.java
@@ -7,8 +7,21 @@ import javax.naming.directory.Attributes;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Mapper for the tokenGroups-attribute data structure in AD. 
+ *
+ * @author Matthias Kauzmann
+ */
 public class AdObjectAttributesMapper implements AttributesMapper<AdObject> {
 
+    /**
+    * Map only the tokenGroups-attribute from AD to an instance of AdObject.
+    * Do this by reading every tokenGroups entry as a byte array, convert it into its 
+    * string representation and channel it into AdObject's tokenGroups field.
+    *
+    * @param attributes that are being fetched from AD
+    * @return an instance of AdObject with a populated tokenGroups field
+    */
     @Override
     public AdObject mapFromAttributes(Attributes attributes) throws NamingException {
 
